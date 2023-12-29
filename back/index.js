ae0b06de5cd38d3d59c761fdca2ef8ba851f161e
Ayushman Gupta <ayushman0021@gmail.com>
2023-12-29 10:48:51 +0530

    q

--- back/index.js
+++ back/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+const UserRouter = require('./routers/userRouter')
+
+const port = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5174'] }));
+app.use( '/user', UserRouter );
+
+
+
+app.listen(port, () => {console.log('Server Started..!!');})
