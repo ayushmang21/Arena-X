182c8af4237c978626da41cc29448d173f9eaf3e
Ayushman Gupta <ayushman0021@gmail.com>
2024-01-14 16:49:05 +0530

    update backend

---
+++ back/routers/tournamentRouter.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const Model = require('../models/tournamentModel');
+
+const router = express.Router();
+
+router.post('/add', (req, res) => {
+
+    console.log(req.body);
+
+    new Model(req.body).save()
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/getall', (req, res) => {
+
+    Model.find()
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
+});
+
+router.put('/update/:id', (req, res) => {
+
+    Model.findByIdAndUpdate(req.params.id, req.body)
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+router.delete('/delete/:id', (req, res) => {
+
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+router.get('/getbyid/:id', (req, res) => {
+
+    Model.findById(req.params.id)
+        .then((result) => {
+            res.json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+router.get('/getpaid', (req, res) => {
+
+    Model.find({entry_type: 'Paid'})
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    
+});
+
+router.get('/getfree', (req, res) => {
+
+    Model.find({entry_type: 'Free'})
+    .then((result) => {
+        res.json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    
+});
+
+
+module.exports = router;
